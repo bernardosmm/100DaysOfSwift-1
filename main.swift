5afeabb2b98defc1d5b1087b4a897f523612c4aa
Bernardo Santos <130390914+bernardosmm@users.noreply.github.com>
2024-03-12 23:50:14 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,25 @@
+// Variables 
+var myFirstVariable: String = "Hello world"
+print(myFirstVariable)
+
+// Strings and Integers
+var name : String = "Bernardo"
+var count : Int = 57
+
+// Doubes and Booleans 
+var height : Double = 1.78
+var echo : Bool = True 
+
+// Multi - line string
+var quotes: String = """
+Down in hole lived a Hobbit
+"""
+print(quotes)
+
+// Interpolation
+var score : Int = 893
+var yourScore : String = "Your score was \(score)"
+print(yourScore)
+
+// Constants 
+let taylor : String = "Swift"
